98a8b4f9c88d94b142f040f2df53d0198deab882
russellfitzpatrick <russellfitzy@gmail.com>
2017-09-20 23:48:55 +0000

    Russell Actually did everything

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ function changeVid(id){
     if (id === "pitt")
     {
         var video = document.getElementById("video");
-        video.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+        video.src = "https://www.youtube.com/embed/6_XRKRcllfc";
     }
     if (id === "hawaii")
     {
         var video = document.getElementById("video");
-        video.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+        video.src = "https://www.youtube.com/embed/FkL6UlnlXcQ";
     }
     if (id === "utah")
     {
         var video = document.getElementById("video");
-        video.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+        video.src = "https://www.youtube.com/embed/B7M1pG2iCb8";
     }
     if (id === "michigan")
     {
         var video = document.getElementById("video");
-        video.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+        video.src = "https://www.youtube.com/embed/I-DQ9imNjG4";
     }
 }
